eb14bf593bc4840a273d3bd8d301918144cb5732
Bavon101 <bavonfate@gmail.com>
2022-05-29 19:52:30 +0300

    build apk

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -21,7 +21,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       key: globalKey,
       appBar: AppBar(
-        title: const Text('D\'s To do'),
+        title: const Text('Dee\'s To do'),
         centerTitle: true,
       ),
       body: Center(
